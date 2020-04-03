8d6dc49fbff402c9e4956be28d5ff72c066ab6e2
vitalykozeev <vitalykozeev@gmail.com>
2020-04-03 17:19:06 +0900

    feat: add show recipe page

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Home from "../components/Home";
 import Recipes from "../components/Recipes";
+import ShowRecipe from "../components/ShowRecipe";
 
 export default (
     <Router>
         <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/recipes" exact component={Recipes} />
+            <Route path="/" exact component={Home}/>
+            <Route path="/recipes" exact component={Recipes}/>
+            <Route path="/recipe/:id" exact component={ShowRecipe}/>
         </Switch>
     </Router>
 );
